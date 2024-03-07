a7a0628867fddf7591ebca33224b34dfb7dbb0f2
camila-falaschi <falaschi.camila@yahoo.com.br>
2024-03-07 14:42:49 -0300

    Fix: Fixing Backend Database Config

--- App/backend/src/database/config/config.js
+++ App/backend/src/database/config/config.js
@@ -1,32 +1,24 @@
 require('dotenv').config();
 
-const suffix = {
-  prod: "",
-  production: "",
-  dev: "-dev",
-  development: "-dev",
-  test: "-test",
-};
+import { Client } from 'pg';
 
-const options = {
+const client = new Client({
+  user: process.env.POSTGRES_USER || 'root',
   host: process.env.POSTGRES_HOST || 'localhost',
-  port: process.env.POSTGRES_PORT || '5432',
-  database: 
-    `${process.env.POSTGRES_NAME || 'productManagementAppDB'}${suffix[environment] || suffix.test}`,
-  username: process.env.POSTGRES_USER || 'root',
+  database: process.env.POSTGRES_NAME || 'productManagementAppDB',
   password: process.env.POSTGRES_PASSWORD || 'password',
-  dialect: process.env.DB_DIALECT || 'postgres',
-  dialectOptions: {
-    timezone: 'Z',
-  },
-  logging: false,
-};
-export const development = {
-    ...options,
-};
-export const test = {
-    ...options,
-};
-export const production = {
-    ...options,
-};
+  port: 5432,
+});
+
+async function connectDatabase() {
+  try {
+    await client.connect();
+    console.log('Conexão bem-sucedida!');
+    return client;
+  } catch (error) {
+    console.error('Erro ao conectar:', error);
+    throw error;
+  }
+}
+
+export default { connectDatabase };
